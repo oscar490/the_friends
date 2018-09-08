91164c5ba246a634750f585692fca13557766642
Óscar Vega Herrera <oscarvegaherrera59@gmail.com>
2018-09-08 16:25:48 +0200

    Mejorando

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,6 +35,7 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
@@ -55,6 +56,7 @@ AppAsset::register($this);
             )
         ],
     ]);
+
     NavBar::end();
     ?>
 
